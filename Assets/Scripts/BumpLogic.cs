352c3577505b348ca7f21f567847a4ec7b6beaaa
LagTheSystem <lagthesystem@duck.com>
2025-11-02 20:00:25 -0600

    Fix singular bomb hitting multiple times

--- Assets/Scripts/BumpLogic.cs
+++ Assets/Scripts/BumpLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BumpLogic : MonoBehaviour
@@ -7,11 +8,13 @@ public class BumpLogic : MonoBehaviour
     public float minimumBump = .5f;
     public float maximumBump = 4f;
     public PlayerController player;
+    private List<GameObject> objects = new List<GameObject>();
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Bomb" && player.isAlive)
+        if (collision.gameObject.tag == "Bomb" && player.isAlive && !objects.Contains(collision.gameObject))
         {
+            objects.Add(collision.gameObject);
             player.transform.Rotate(new Vector3(Mathf.Sign(collision.GetContact(0).point.z) * Mathf.Clamp(Mathf.Abs(collision.GetContact(0).point.z), minimumBump, maximumBump) * bumpStrength, 0, 0));
         }
     }
